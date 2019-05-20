0fcc0202d12fef082e17588fd3f3e68a3329d59a
James Hylins <james.hylins@johnlewis.co.uk>
2019-05-20 14:21:54 +0000

    new test for divisable by 10

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -16,4 +16,9 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed any number divisible by 3' do
     expect(fizzbuzz(6)).to eq'fizz'
   end
+
+  it 'returns "buzz" when passed any number divisible by 5' do
+    expect(fizzbuzz(10)).to eq'buzz'
+  end
+
 end
